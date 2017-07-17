14445898959aa8bd032a0210598c90ad4feab3cb
Luís_ <luisfonseca720@gmail.com>
2017-07-17 17:24:04 +0100

    Update and rename start_python_script.bat to start_python_script-example.bat

---
+++ start_python_script-example.bat
@@ -0,0 +1,13 @@
+python 'Path to ethereum.py' 'Path to ethminer.exe' 'Exact name of the ethminer process' 'Ethminer parameters' 'Ethereum adress' 'Time between checks'
+pause
+
+
+:: EXAMPLE
+
+:: python "C:\Program Files\Ethminer\ethereum.py" "C:\Program Files\Ethminer\ethminer.exe" ethminer.exe "--farm-recheck 1000 -U -S eth-eu1.nanopool.org:9999 -FS eth-eu2.nanopool.org:9999 -O 0xc5c5a034db718ce4abb6971c860d10aed74833aC" 0xc5c5a034db718ce4abb6971c860d10aed74833aC 10
+:: pause
+
+:: IMPORTANT
+
+:: If there is any space between a parameter QUOTATION MARKS ARE NEEDED
+:: If any of the parameters is not filled the script will not work properly
